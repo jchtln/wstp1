8dfae7f02295c99bc53025489e23069a8792b614
o21910235@campus.univ-orleans.fr <jchtln>
2022-01-25 11:05:20 +0100

    Exercice 1

---
+++ exercice1/exo1/IdentityData.java
@@ -0,0 +1,20 @@
+public class IdentityData {
+    private String prenom;
+    private String nom;
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
---
+++ exercice1/exo1/LocationData.java
@@ -0,0 +1,29 @@
+public class LocationData {
+    private String adresse;
+    private Integer CodePostal;
+    private String ville;
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Integer getCodePostal() {
+        return CodePostal;
+    }
+
+    public void setCodePostal(Integer codePostal) {
+        CodePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+}
