78a660a122406393bd02e3cbe19dc98aa3edc3b2
o21910235@campus.univ-orleans.fr <jchtln>
2022-02-01 12:15:23 +0100

    Exercice 2.1

--- exercice2/motusclient/src/main/java/Main.java
+++ exercice2/motusclient/src/main/java/Main.java
@@ -10,10 +10,16 @@ public class Main {
 
     public static void main(String[] args) throws PseudoDejaPrisException, TicketInvalideException, PartieInexistanteException, MaxNbCoupsException, MotInexistantException {
         MotusProxy motusProxy = new MotusProxyImpl();
-        String tokenAuthentification = motusProxy.creerUnCompte("Yohan");
+        String tokenAuthentification = motusProxy.creerUnCompte("Julin");
+
+        System.out.println("token:"+tokenAuthentification);
+
+        //motusProxy.creerUnCompte("Yohan");
+
         String tokenPartie = motusProxy.creerUnePartie(tokenAuthentification);
-        List<String> tentatives = motusProxy.getPropositions(tokenPartie);
+        System.out.println("token:"+tokenPartie);
+       List<String> tentatives = motusProxy.getPropositions(tokenPartie);
         EtatPartie etat = motusProxy.proposerMot(tokenPartie,"acheter");
-        tentatives = motusProxy.getPropositions(tokenPartie);
+       //tentatives = motusProxy.getPropositions(tokenPartie);
     }
 }
--- exercice2/motusclient/src/main/java/proxy/MotusProxyImpl.java
+++ exercice2/motusclient/src/main/java/proxy/MotusProxyImpl.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import exceptions.*;
 import proxy.dto.EtatPartie;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 public class MotusProxyImpl implements MotusProxy{
@@ -17,26 +19,117 @@ public class MotusProxyImpl implements MotusProxy{
 
     @Override
     public String creerUnCompte(String pseudo) throws PseudoDejaPrisException {
-        HttpRequest request = HttpRequest.newBuilder().build()
-                .uri(URI.create("http://localhost:8080/motus/joueur?pseudo"+pseudo))
-                .POST(HttpRequest.BodyPublishers.noBody())
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/motus/joueur"))
+                .setHeader("Content-Type","application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString("pseudo="+pseudo))
                 .build();
+
+
+        try {
+            HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if(httpResponse.statusCode()==201){
+                String resultat = httpResponse.headers().firstValue("token").get();
+                return resultat;
+            }
+            else {
+                throw new PseudoDejaPrisException();
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Soucis");
+        }
 
     }
 
+
     @Override
     public String creerUnePartie(String tokenAuthentification) {
-        return null;
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/motus/partie"))
+                .header("token","tokenAuthentification")
+                .POST(HttpRequest.BodyPublishers.ofString(""))
+                .build();
+
+        try {
+            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            switch (httpResponse.statusCode()){
+
+                case 201 : {
+                    String resultat = httpResponse.headers().firstValue("tokenPartie").get();
+                    return resultat;
+                }
+
+                case 400 | 401 : {
+                    throw new TicketInvalideException();
+                }
+
+                default: {
+                    throw new RuntimeException("Code retour inattendu");
+                }
+            }
+
+        } catch (IOException | InterruptedException | TicketInvalideException e) {
+            throw new RuntimeException("SOUCIS");
+        }
+
     }
 
     @Override
     public EtatPartie proposerMot(String tokenPartie, String proposition) throws MotInexistantException, MaxNbCoupsException, TicketInvalideException {
-        return null;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/motus/partie"))
+                .header("tokenPartie", tokenPartie)
+                .header("Accept","application/json")
+                .header("Content-Type","application/x-www-form-urlencoded")
+                .PUT(HttpRequest.BodyPublishers.ofString("proposition="+proposition))
+                .build();
+
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+            switch (response.statusCode()) {
+
+                case 200: {
+                    String body = response.body();
+                    EtatPartie etatPartie = this.objectMapper.readValue(body,EtatPartie.class);
+                    return etatPartie;
+                }
+
+                case 404: {
+                    throw new MotInexistantException();
+                }
+
+                case 406: {
+                    throw new MaxNbCoupsException();
+                }
+
+                case 400: {
+                    throw new TicketInvalideException();
+                }
+
+                default: {
+                    throw new RuntimeException("Code inconnu");
+                }
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("ploufff");
+        }
+
     }
 
     @Override
     public List<String> getPropositions(String tokenPartie) throws TicketInvalideException, PartieInexistanteException {
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("http://localhost:8080/motus/partie?tokenPartie"))
+//                .GET(HttpRequest.BodyPublishers)
         return null;
-    }
+   }
 
 }
